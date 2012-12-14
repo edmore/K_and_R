50e13ca3a03f8a481fe518090b90e1a2f5be4126
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-12-14 21:40:25 +0200

    Printing out the conversion in reverse

--- chapter_1/fahr_to_celsius.c
+++ chapter_1/fahr_to_celsius.c
@@ -4,11 +4,11 @@
 
 int main(int argc, char *argv[]){
   float celsius;
-  int fahr = 0;
-  int limit = 300;
+  int fahr;
+  int limit = 0;
   int step = 20;
 
-  for(fahr = 0; fahr <= limit ; fahr+=step){
+  for(fahr = 300; fahr >= limit ; fahr-=step){
     celsius = (5.0/9.0) * (fahr - 32);
     printf("%3d\t%6.1f\n", fahr, celsius);
   }
