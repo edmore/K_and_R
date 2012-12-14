5997859ab08d8fc42ffbb9f58a062012e5c06897
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-12-14 22:41:12 +0200

    Adding in symbolic constants

--- chapter_1/fahr_to_celsius.c
+++ chapter_1/fahr_to_celsius.c
@@ -1,14 +1,15 @@
 /* Fahrenheit to Celsius conversion */
 
 #include <stdio.h>
+#define STEP 20
+#define LOWER 0
+#define MAX 300
 
 int main(int argc, char *argv[]){
   float celsius;
   int fahr;
-  int limit = 0;
-  int step = 20;
 
-  for(fahr = 300; fahr >= limit ; fahr-=step){
+  for(fahr = LOWER; fahr <= MAX ; fahr+=STEP){
     celsius = (5.0/9.0) * (fahr - 32);
     printf("%3d\t%6.1f\n", fahr, celsius);
   }
