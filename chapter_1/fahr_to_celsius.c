40a841998a96a706dd71009c8f08b6dd5fa5615b
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-12-14 20:28:54 +0200

    More refactoring

--- chapter_1/fahr_to_celsius.c
+++ chapter_1/fahr_to_celsius.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
   int limit = 300;
   int step = 20;
 
-  for(fahr = 0; fahr <= limit ; fahr=fahr+step){
+  for(fahr = 0; fahr <= limit ; fahr+=step){
     celsius = (5.0/9.0) * (fahr - 32);
     printf("%3d\t%6.1f\n", fahr, celsius);
   }
