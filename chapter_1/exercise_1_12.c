4d8ba473d0dadb48e1a538b81454af9398c0b6c6
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-12-17 21:56:34 +0200

    Print input in its own line

---
+++ chapter_1/exercise_1_12.c
@@ -0,0 +1,14 @@
+/* print input one word per line */
+
+#include <stdio.h>
+
+main(){
+  int c;
+
+  while((c = getchar()) != EOF){
+    if(c == '\n' || c == ' ' || c == '\t')
+      printf("\n");
+    else
+      putchar(c);
+  }
+}
