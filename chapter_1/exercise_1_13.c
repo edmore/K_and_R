9d1003cc28a37097489efd9b79a0d06c00d5cb9e
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-12-19 20:57:08 +0200

    initialize the array with zeros

--- chapter_1/exercise_1_13.c
+++ chapter_1/exercise_1_13.c
@@ -7,8 +7,12 @@ main(){
   int c;
   int nc = 0;
   int nw = 0;
-  int i,k;
+  int i,j,k;
   int histogram[MAX];
+
+  // initialize array to array of zeros
+  for (j = 0; j < MAX; j++)
+    histogram[j] = 0;
 
   printf("Please input a sentence ( maximum %d words ) and press enter when you are done:\n", MAX);
   // exit the loop if the user enters a newline character
@@ -25,10 +29,12 @@ main(){
 
   printf("\n");
   printf("Histogram of words:\n");
-  for (i = 0; i <= nw; ++i){
-    printf("%2.0d|", histogram[i]);
-    for(k = 0; k < histogram[i]; ++k)
-      printf("=");
-    printf("\n");
-  }
+
+    for (i = 0; i <= nw; ++i){
+      printf("%2.0d|", histogram[i]);
+      for(k = 0; k < histogram[i]; ++k)
+        printf("=");
+      printf("\n");
+    }
+
 }
