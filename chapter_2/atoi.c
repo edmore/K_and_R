00278f3b0c14044971734dcab2f6478832996d2b
Edmore Moyo <etmoyo@gmail.com>
2013-01-19 23:30:20 +0200

    K&R atoi

---
+++ chapter_2/atoi.c
@@ -0,0 +1,25 @@
+/* Convert string of digits into its numeric equivalent - K & R */
+
+#include <stdio.h>
+
+int atoi(char s[]);
+
+int main()
+{
+  char s[3] = "412";
+  printf("%i\n", atoi(s));
+  return 0;
+}
+
+
+int atoi(char s[])
+{
+  int i,n;
+
+  n=0;
+
+  for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+    n = 10 * n + (s[i] - '0');
+
+  return n;
+}
