8c4ea6bad593c121069643fd5284a37d334433f7
Edmore Moyo <etmoyo@gmail.com>
2013-01-22 09:34:56 +0200

    refactoring extra variable not required

--- chapter_2/exercise_2_3.c
+++ chapter_2/exercise_2_3.c
@@ -12,7 +12,6 @@ int htoi(char s[]);
 int main()
 {
   int i = 0;
-  int j = 0;
   int value;
   char c;
   char s[MAXLINE];
@@ -22,13 +21,11 @@ int main()
     if ( c == '0' || (c >= 'g' && c <= 'z') ){
       ;
     }else{
-      s[i] = c;
-      ++j;
-      ++i;
+      s[i++] = c;
     }
   }
 
-  count = j;
+  count = i;
   value = htoi(s);
   printf("The hex string as an integer is %i.\n", value);
   return 0;
