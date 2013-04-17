9fc2ff8bc800727e5ef7b16ecf8a2cff51d1496f
Edmore Moyo <etmoyo@gmail.com>
2013-04-17 21:59:18 +0200

    pointers 101

---
+++ chapter_5/swap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *, int *);
+
+void swap(int *p, int *q)
+{
+  int temp;
+  temp = *p; // salvage the value of int that p points to
+  *p = *q;
+  *q = temp;
+}
+
+int main(void)
+{
+
+  int x, y;
+  x = 1;
+  y = 2;
+
+  printf("x is %i and y is %i.\n", x, y);
+  // swap
+  swap(&x, &y);
+  printf("x is %i and y is %i.\n", x, y);
+
+  return 0;
+}
