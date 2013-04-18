8bf6393826155b052ff17ae356f4266246ea95b6
Edmore Moyo <etmoyo@gmail.com>
2013-04-18 14:01:14 +0200

    separating the functions for better clarity

---
+++ chapter_5/pointers_and_arrays.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char name[] = "Edmore";
+  char * pa;
+  // name of array is synonym for first element &a[0]
+  pa = name; // pointer is a variable, array name isnt
+
+  // arrays and pointers 101
+  printf("the first character is %c\n", *pa);
+  printf("the preceding character is %c\n", *(pa+1)); //contents of the second element
+  // interesting : C actually evaluates arrays like so; equivalent to top statement
+  printf("the preceding character is %c\n", *(name+1)); //contents of the second element
+
+  // printing out all elements in name:
+  while (*pa != '\0'){
+    printf("%c", *pa);
+    pa++;
+  }
+
+  return 0;
+}
--- chapter_5/swap.c
+++ chapter_5/swap.c
@@ -13,7 +13,6 @@ void swap(int *p, int *q)
 int main(void)
 {
   int x, y;
-  char name[] = "Edmore";
   x = 1;
   y = 2;
 
@@ -21,22 +20,6 @@ int main(void)
   // swap
   swap(&x, &y);
   printf("x is %i and y is %i.\n", x, y);
-
-  char * pa;
-  // name of array is synonym for first element &a[0]
-  pa = name; // pointer is a variable, array name isnt
-
-  // arrays and pointers 101
-  printf("the first character is %c\n", *pa);
-  printf("the preceding character is %c\n", *(pa+1)); //contents of the second element
-  // interesting : C actually evaluates arrays like so; equivalent to top statement
-  printf("the preceding character is %c\n", *(name+1)); //contents of the second element
-
-  // printing out all elements in name:
-  while (*pa != '\0'){
-    printf("%c", *pa);
-    pa++;
-  }
 
   return 0;
 }
