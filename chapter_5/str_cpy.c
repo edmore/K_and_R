398ea9be3b4cc2c23202132bdd2e66238a34f606
Edmore Moyo <etmoyo@gmail.com>
2013-05-15 20:54:47 +0200

    Renaming file

---
+++ chapter_5/str_cpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void str_cpy(char *, char *);
+
+void str_cpy(char *s, char *t)
+{
+  // no need to use '\0', '\0' also copied.
+  while ((*s++ = *t++))
+    ;
+}
+
+int main(void)
+{
+  char name[] = "Edmore";
+  char surname[] = "Moyo";
+
+  str_cpy(name, surname);
+  printf("Name is %s, surname is %s\n", name, surname);
+  return 0;
+}
