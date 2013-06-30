6eea2d957e4a4ce2cc8cbd12100e120129217f7f
Edmore Moyo <etmoyo@gmail.com>
2013-06-30 09:20:45 +0200

    structures 101

---
+++ chapter_6/struct_101.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct point {
+  int x;
+  int y;
+};
+
+int main(void)
+{
+
+  struct point pt, *pp = &pt;
+  pt.x = 3;
+
+  printf("%d\n", (*pp).x);
+  // same as ...
+  printf("%d\n", pp->x);
+  return 0;
+}
